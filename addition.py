cf4d152226d6edaac498ce05ade52b0b5ff3e48a
Pranav <pranav@expertzlab.com>
2026-03-02 12:50:49 +0530

    project files are modified

--- addition.py
+++ addition.py
@@ -1,3 +1,7 @@
 a = 50
 b = 60 
 print("Total ==", a+b)
+
+c = 500
+d = 600
+print("Total ==", c+d)
--- file1.py
+++ file1.py
@@ -1,2 +1,3 @@
 print("Hello welcome to Git...!")
-
+print("Hey all good afternoon")
+print("Hi how are you..?")
